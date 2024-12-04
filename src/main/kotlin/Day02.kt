ce3c78a1685cb867a9395e871a141e5bc1962f60
MarcNME <marc.enzmann@satis-faction.de>
2024-12-04 23:56:24 +0100

    Added Part 2 of day 2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -4,15 +4,32 @@ fun main() {
     val numbers = input.map { line -> line.split(' ').map { it.toInt() } }
 
     var numSafeReports = 0
+    var numSaveWithDampener = 0
 
     numbers.forEach { report ->
-        println("$report Safe decreasing: ${report.levelsDecreasing()} Safe increasing: ${report.levelsIncreasing()}")
         if (report.isSafe()) {
             numSafeReports++
+            println("$report without removing any level")
+        } else if (report.isSaveWithDampener()) {
+            numSaveWithDampener++
         }
+
     }
 
     println("Safe reports: $numSafeReports")
+    println("Safe with dampener: ${numSafeReports + numSaveWithDampener}")
+}
+
+private fun List<Int>.isSaveWithDampener(): Boolean {
+    for (i in this.indices) {
+        val mutableReport = this.toMutableList()
+        mutableReport.removeAt(i)
+        if (mutableReport.isSafe()) {
+            println("$this is save by removing ${this[i]}")
+            return true
+        }
+    }
+    return false
 }
 
 fun List<Int>.levelsDecreasing(): Boolean {
